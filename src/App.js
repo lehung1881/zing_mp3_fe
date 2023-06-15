3efe3fc3f003173cd7ced1e0f9ca028b6f264ee5
Le Hung <hungle180801@gmail.com>
2023-06-15 22:32:33 +0700

    Cập nhật lại source, thêm router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Home, Login, Public } from "./containers/public/index.js";
+import { Routes, Route } from "react-router-dom";
+import path from "./utils/path.js";
 
 function App() {
   return (
     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Routes>
+        <Route path = {path.PUBLIC} element = { <Public/> }>
+          <Route path = {path.HOME} element = { <Home/> }/>
+          <Route path = {path.LOGIN} element = { <Login/> }/>
+        </Route>
+      </Routes>
+    </h1>
   );
 }
 
--- src/stores/reducers/AppReducer.js
+++ src/stores/reducers/AppReducer.js
@@ -9,7 +9,7 @@ const appReducer = (state = initState, action) => {
     case ActionType.GET_HOME:
       return state;
     default:
-      return
+      return state;
   }
 }
 
